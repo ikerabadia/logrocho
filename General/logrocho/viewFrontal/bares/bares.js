0c7af48f77b4d834b4046b96b28cd771981823a8
Iker Abadia <ikerabadia999@gmail.com>
2022-02-18 19:50:26 +0100

    listado de bares se pinta

---
+++ General/logrocho/viewFrontal/bares/bares.js
@@ -0,0 +1,86 @@
+window.onload = function() {
+    mostrarDatos();
+};
+
+function mostrarDatos() {
+    var fTextoBuscador = document.getElementById("buscadorInput").value;
+    var fNotaMinima = document.getElementById("inputNotaMinima").value;
+    var fNotaMaxima = document.getElementById("inputNotaMaxima").value;
+    var settings = {
+        "url": "http://localhost/logrocho/index.php/api/restaurantesFiltrados",
+        "method": "POST",
+        "timeout": 0,
+        "headers": {
+          "Content-Type": "application/x-www-form-urlencoded"
+        },
+        "data": {
+          "fTextoBuscador": ""+fTextoBuscador,
+          "fNotaMinima": ""+fNotaMinima,
+          "fNotaMaxima": ""+fNotaMaxima
+        }
+      };
+      
+      $.ajax(settings).done(function (response) {
+        var json = response;
+        var resultados=eval(json);
+
+        document.getElementById("contenedorBares").innerHTML = "";
+        var bar;
+        //Asigno los datos
+        for (let i = 0; i < resultados["bares"].length; i++) {
+            bar = resultados["bares"][i];
+            document.getElementById("contenedorBares").innerHTML += ""+
+            "<a href=\"barFront/"+bar["idRestaurante"]+"\">"+
+                    "<div class=\"bar\" id=\"bar\""+bar["idRestaurante"]+">"+
+                        "<div class=\"datosBar\">"+
+                            "<div class=\"nombrePuntuacionBar\">"+
+                                "<div class=\"nombreBar\">"+bar["nombre"]+"</div>"+
+                                "<div class=\"puntuacionBar\">"+getEstrellasPuntuacion(bar["nota"])+"</div>"+
+                            "</div>"+
+                            "<div class=\"localizacionBar\">"+bar["localizacion"]+"</div>"+
+                        "</div> "+            
+                    "</div>"+
+                "</a>";
+        }
+        //Asigno las imagenes
+        for (let i = 0; resultados["bares"].length; i++) {
+            
+            var settings = {
+                "url": "http://localhost/logrocho/index.php/api/getImagenRestaurante",
+                "method": "POST",
+                "timeout": 0,
+                "headers": {
+                "Content-Type": "application/x-www-form-urlencoded"
+                },
+                "data": {
+                "idBar": ""+resultados["bares"]["idRestaurante"],
+                "numeroImagen": "1"
+                }
+            };
+            
+            $.ajax(settings).done(function (response) {
+                var json = response;
+                var resultados=eval(json);
+                document.getElementById("bar"+resultados["bares"]["idRestaurante"]).style.background("url(\""+resultados["imagenes"][0]["imagen"]+"\")")
+            });
+            
+        }
+      });
+      
+}
+
+function getEstrellasPuntuacion(puntuacion) {
+    if (puntuacion<2 || puntuacion == null) {
+        return "☆☆☆☆☆";
+    }else if(puntuacion<4){
+        return "★☆☆☆☆";
+    }else if(puntuacion<6){
+        return "★★☆☆☆";
+    }else if(puntuacion<8){
+        return "★★★☆☆";
+    }else if(puntuacion<9.5){
+        return "★★★★☆";
+    }else if(puntuacion>=9.5){
+        return "★★★★★";
+    }
+}
